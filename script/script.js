ef04c677072898a50bf620f1b09c4f11fa8f5eb6
fares houidi <fereshouidi298@gmail.com>
2024-10-19 19:41:37 +0100

    tntn

--- script/script.js
+++ script/script.js
@@ -21,29 +21,64 @@ const project_list = [
     {
         id: 1,
         tittle: 'messager web app',
-        image: 'images/messager_web_app.jpg',
+        image: ['images/messager_web_app.jpg', ''],
         short_discription: "application for chatting",
+        about: 'example about',
+        discription: 'example discription'
     },
     {
         id: 2,
         tittle: 'app 2',
-        image: 'images/profileIMG2.jpg',
+        image: ['images/profileIMG2.jpg'],
         short_discription: "Script Writing & Video Production",
+        about: 'example about',
+        discription: 'example discription'
     },
     {
         id: 3,
         tittle: 'app 3',
-        image: 'images/profileIMG2.jpg',
+        image: ['images/profileIMG2.jpg'],
         short_discription: "Script Writing & Video Production",
+        about: 'example about',
+        discription: 'example discription'
     },
 ]
 
 const show_more_detail = (id, index) => {
     const more_details_div = document.getElementById("more-detail");
-        more_details_div.style.display = 'flex';
-        //alert(project_list[index].tittle);
+    const images_container = document.getElementById('images-container');
+    more_details_div.style.display = 'flex';
     
-}
+    let imagesHTML = '';
+    project_list[index].image.forEach((image_) => {
+        imagesHTML += `<img src="${image_}" alt="">`;
+    });
+
+    const container_div = `
+      <div id="content">
+        <div id="project-details-header">
+          <h2 id="project-name">${project_list[index].tittle}</h2>
+          <i class="fas fa-times" onclick="close_more_detail()"></i>
+        </div>
+        <div id="about">
+          <h2>about</h2>
+          <p>${project_list[index].about}</p>
+        </div>
+        <div id="images">
+          <h2>images</h2>
+          <div id="images-container"> 
+              ${imagesHTML}
+          </div>
+        </div>
+        <div id="discription">
+          <h2>discription</h2>
+          <p>${project_list[index].discription}</p>
+        </div>
+      </div>`;
+
+    more_details_div.innerHTML = container_div;
+};
+
 const close_more_detail = () => {
     const more_details_div = document.getElementById("more-detail");
         more_details_div.style.display = 'none';
@@ -51,26 +86,13 @@ const close_more_detail = () => {
 }
 
 
-// let more_details_div_showing = false;
-// const handle_more_detail = (id, index) => {
-//     const more_details_div = document.getElementById("more-detail");
-//     if(more_details_div_showing){
-//         more_details_div.style.display = 'none';
-//         more_details_div_showing = false;
-//     }else{
-//         more_details_div.style.display = 'flex';
-//         more_details_div_showing = true;
-//     }
-//     //alert(project_list[index].tittle);
-    
-// }
 
 const project_content = document.getElementById("project-content")
 
 project_list.map((project, index) => {
     const project_div = `<div class="project-card">
     <div class="project-image">
-      <img class="img_prg" src=${project.image}>
+      <img class="img_prg" src=${project.image[0]}>
     </div>
     <div class="project-info">
       <span class="s">${project.short_discription} </span>
